d8c140bce35184b1e7c32501ae1343cda2b7da88
spirit <catchspirit37@gmail.com>
2018-11-14 12:02:55 +0800

    z - Wed Nov 14 2018 12:02:55 GMT+0800 (CST)

---
+++ bin/postinstall.js
@@ -0,0 +1,21 @@
+const {CliUtil} = require('@ys/collection')
+const {execSync} = CliUtil
+const path = require('path')
+const rootDir = path.resolve(__dirname, '../')
+const resource = path.resolve(rootDir, 'resource')
+const sqliteNodeFileName = 'node_sqlite3.node'
+const sqliteElectronFolder = path.resolve(rootDir, 'node_modules/sqlite3/lib/binding/electron-v3.0-darwin-x64')
+const sqliteNode = path.resolve(sqliteElectronFolder, sqliteNodeFileName)
+const fse = require('fs-extra')
+
+const option =  {
+  cwd: rootDir
+}
+if (!fse.existsSync(sqliteNode)) {
+  fse.ensureDirSync(sqliteElectronFolder)
+  fse.copySync(path.resolve(resource, sqliteNodeFileName), path.resolve(sqliteElectronFolder, sqliteNodeFileName))
+  // execSync(`npm run install:sqlite3`, option)
+}
+execSync(`npm run asars && npm run build`, option)
+
+console.log('postinstall finished')
