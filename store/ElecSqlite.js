b0b9142c0e675760539a8f087cce74ae00ddf74e
spirit <catchspirit37@gmail.com>
2018-11-21 16:03:51 +0800

    z - Wed Nov 21 2018 16:03:51 GMT+0800 (CST)

--- store/ElecSqlite.js
+++ store/ElecSqlite.js
@@ -1,16 +1,20 @@
 
 const path = require("path");
-const sqlite3 = require("../asars/sqlite3.asar");
+const sqlite3 = require("../asars/sqlite3.asar")
+const fse = require('fs-extra')
 
 let dbName = engine.getApplication().getCurrentApp().getName()||"default";
 
 let db = new sqlite3.cached.Database(path.join(__dirname, dbName+".db"));
 
-db.saveFile = function (filePath,fileName,data) {
-    return new Promise((resolve,reject)=>{
-        //TODO
-        resolve()
-    });
+db.saveFile =  (filePath,fileName,data) => {
+  return new Promise((resolve)=>{
+    const folder = path.resolve(__dirname, '..') + filePath
+    fse.ensureDirSync(folder)
+    const url = path.resolve(folder, fileName)
+    fse.writeFileSync(url, data, 'base64')
+    resolve(url)
+  })
 
 }
 
