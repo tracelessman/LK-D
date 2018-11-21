cec0a4f31b672f68b2c3bc76a32a966b2c92dd8f
spirit <catchspirit37@gmail.com>
2018-11-21 16:08:45 +0800

    z - Wed Nov 21 2018 16:08:45 GMT+0800 (CST)

--- store/ElecSqlite.js
+++ store/ElecSqlite.js
@@ -9,7 +9,7 @@ let db = new sqlite3.cached.Database(path.join(__dirname, dbName+".db"));
 
 db.saveFile =  (filePath,fileName,data) => {
   return new Promise((resolve)=>{
-    const folder = path.resolve(__dirname, '..') + filePath
+    const folder = path.resolve(__dirname, '../external') + filePath
     fse.ensureDirSync(folder)
     const url = path.resolve(folder, fileName)
     fse.writeFileSync(url, data, 'base64')
