e516a8828b5b43a1303889d1b7852eb76de1a83e
spirit <catchspirit37@gmail.com>
2019-03-17 10:44:33 +0800

    z

--- config/unversioned.js
+++ config/unversioned.js
@@ -1,6 +1,6 @@
 const config = {
-    checkUpdateUrl: 'http://localhost:3000/users',
-    minHeight: 0,
-    minWidth: 0
+    // checkUpdateUrl: 'http://localhost:3000/users',
+  minHeight: 0,
+  minWidth: 0
 }
 module.exports = config
