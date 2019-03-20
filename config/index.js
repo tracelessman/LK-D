9e38303bf030bed8e8aeb41db5d924d99fd19e31
spirit <catchspirit37@gmail.com>
2019-03-20 10:23:13 +0800

    z - Wed Mar 20 2019 10:23:12 GMT+0800 (China Standard Time)

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const fs = require('fs')
-const _ = require('lodash')
+// const _ = require('lodash')
 
 const {getGoldenHeight} = require('../util/Independent')
 
@@ -14,9 +14,9 @@ const config = {
   checkUpdateTimeout: 1000 * 5
 }
 
-const unversionedPath = path.resolve(__dirname, 'unversioned.js')
-if (fs.existsSync(unversionedPath)) {
-  _.merge(config, require(unversionedPath))
-}
+// const unversionedPath = path.resolve(__dirname, 'unversioned.js')
+// if (fs.existsSync(unversionedPath)) {
+//   _.merge(config, require(unversionedPath))
+// }
 Object.freeze(config)
 module.exports = config
