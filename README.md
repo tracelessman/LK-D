348b523b34d399fad6caad5ce8000fa79221bed0
gitpanyu <1224721916@qq.com>
2019-03-27 13:43:54 +0800

    删除消息窗口

--- README.md
+++ README.md
@@ -4,3 +4,11 @@
 ```
 node bin/openDb
 ```
+## 分支
+```
+开发环境分支（dev分支）
+生产环境分支 (publish分支)
+稳定分支 （master分支）
+```
+
+
---
+++ upload.js
@@ -0,0 +1,33 @@
+const fs = require('fs')
+const path = require('path')
+const node_ssh = require('node-ssh')
+const ssh = new node_ssh()
+const password = 'abc@147258369'
+const username = 'root'
+const host = '193.112.250.147'
+
+const cmd = 'cd /root/Test/LK-S;git pull;cd static/public/LK-D;git pull'
+
+function upload() {
+    ssh.connect({
+        host,
+        username,
+        port: 22,
+        password,
+    })
+        .then(function () {
+            ssh.exec(cmd, [], {
+                onStdout(chunk) {
+                    console.log('stdoutChunk', chunk.toString('utf8'))
+                },
+                onStderr(chunk) {
+                    console.log('stderrChunk', chunk.toString('utf8'))
+                },
+            })
+        })
+}
+
+module.exports = upload()
+
+
+
